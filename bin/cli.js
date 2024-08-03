37a1ffba5928a2f00edd25ce3e8151bb40424b36
Nick Thomas <nichoth@nichoth.com>
2024-08-03 16:27:11 -0700

    rebuild

--- bin/cli.js
+++ bin/cli.js
@@ -43,7 +43,7 @@ const parsed = JSON.parse(packageJson);
 delete parsed.scripts["build-cli"];
 await fs.writeFile(packagePath, JSON.stringify(parsed, null, 2));
 const readmePath = path.resolve(__dirname, "..", "README.example.md");
-const readmeTmpl = Handlebars.compile(await fs.readFile(readmePath));
+const readmeTmpl = Handlebars.compile("" + await fs.readFile(readmePath));
 const newReadme = readmeTmpl(templateParams);
 await fs.writeFile(path.resolve(__dirname, "README.md"), newReadme);
 await fs.rm(path.resolve(path.resolve(__dirname)), {
